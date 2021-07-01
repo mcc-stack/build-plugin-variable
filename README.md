c7a1fa176204344958e6f6f22b6731c0113c348a
mcc01032269 <mcc01032269@alibaba-inc.com>
2021-07-01 17:35:11 +0800

    feat: update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # README
 
-ice.js 用于加载 scss 全局变量的 webpack 插件
+ice.js 用于加载 scss 全局变量(src 目录下的\_variable.scss)的 webpack 插件
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 import { IPlugin } from '@alib/build-scripts';
+import Config from 'webpack-chain';
 
-const plugin: IPlugin = ({ onGetWebpackConfig }, css) => {
+const plugin: IPlugin = ({ onGetWebpackConfig }) => {
+  function addSassResourcesLoader(configRule: Config.Rule) {
+    configRule
+      .use('sass-resources-loader')
+      .loader(require.resolve('sass-resources-loader'))
+      .options({
+        resources: process.cwd() + '/src/_variable.scss'
+      });
+  }
   onGetWebpackConfig(config => {
-    // ['scss', 'scss-module'].forEach(rule => {
-    ['scss'].forEach(rule => {
-      config.module
-        .rule(rule)
-        .use('sass-resources-loader')
-        .loader(require.resolve('sass-resources-loader'))
-        .options({
-          resources: process.cwd() + '/src/_variable.scss'
-        });
-    });
+    const sassRegex = /\.(scss|sass)$/;
+    const sassModuleRegex = /\.module\.(scss|sass)$/;
+    addSassResourcesLoader(config.module.rule('scss').test(sassRegex));
+    addSassResourcesLoader(
+      config.module.rule('scss-module').test(sassModuleRegex)
+    );
   });
 };
 
